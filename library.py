2683e693cbf8b9d22ebd534308e85c9411fad9ef
Jaden <jaden07edwards@gmail.com>
2025-12-05 13:05:14 -0600

    pseudo1

---
+++ library.py
@@ -0,0 +1,25 @@
+import mysql.connector
+
+#libdb = mysql.connector.connect(
+#    host="localhost",
+#    user="root",
+ #   password="..."
+#)
+
+#cursor = libdb.cursor()
+
+### PSEUDOCODE
+#user class??
+
+Add_User():
+    input("what type of user? (lib or reader)")
+    input("Username:")
+    input("password:")
+
+Login()
+    input ("user and pass")
+    if pass.user == user.user 
+        let in
+    else    
+        prompt again
+
