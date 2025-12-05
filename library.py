5184e439e6b0074b40ca9e8ff71bc69bf96128a0
Jaden <jaden07edwards@gmail.com>
2025-12-05 13:25:01 -0600

    pseudofin

--- library.py
+++ library.py
@@ -23,3 +23,31 @@ Login()
     else    
         prompt again
 
+Delete_User()
+    if (lib == true)
+        del chosen user 
+    if (user_acct == user_acct_del)
+        del chosen user 
+
+Add_book()
+    if (lib == true)
+        add books, publisher, author
+        to respective tables
+
+Delete_book()
+    if (lib == true)
+        delete books (and all info connected)
+
+Check_out_book()
+    if(check if book is NUll in checked out)
+        update checked_out to True
+        update checked_out to current_date
+
+Return_book()
+    if (book is not NULL )
+        update checked_out to False
+        check_in_date update (?)
+                              
+Modify_user()
+    if (librarian == true or user_acct == act_user)
+        update user attributes
