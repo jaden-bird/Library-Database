5655d18b502612ccfe98980578a169c309bab2ce
Jaden <jaden07edwards@gmail.com>
2025-12-06 21:22:27 -0600

    delbook

--- library.py
+++ library.py
@@ -10,15 +10,84 @@ libdb = mysql.connector.connect(
 cursor = libdb.cursor()
 rflag = False
 lflag = False
-
-#Lets user add an account, the specifics will be inserted
-#into the User table 
+ 
 def Welcome_Screen():
     status = input("Welcome to our library database, are you a new or returning user?:")
     if status == "new":
         Add_User()
     else:
         Login()
+
+#Lets user add an account, the specifics will be inserted
+#into the User table
+
+#Login Menu
+def Login():
+    global current_user
+    current_user = None
+    print("Login")
+    username = input("Username:")
+    upassword = input("Password:")
+
+    cursor.execute(
+        "SELECT * FROM Users WHERE username = %s and upassword = %s",
+        (username, upassword)
+    )
+    #fetches complete line form db
+    name = cursor.fetchone()
+    if name:
+        current_user = username
+        Menu()
+    else:
+        print("Incorrect user or password")
+        Welcome_Screen() 
+
+#Lets the user add a book and its details 
+def Add_Book():
+    checked_out_by = current_user
+    title = input("What is the book title?: ")
+    aid = input("What is the author's ID?: ")
+    date_published = input("What date was it published? ('yyyy-mm-dd'): ")
+    check_out_date = input("What is the date? ('yyyy-mm-dd'): ")
+    author = input("Who is the author?: ")
+
+    cursor.execute(
+        "INSERT INTO Book(title, aid, date_published, checked_out_by, check_out_date) VALUES (%s, %s, %s, %s, %s)",
+        (title, aid, date_published, checked_out_by, check_out_date)
+    )
+    libdb.commit()
+
+def Delete_Book():
+    title = input("Which book would you like to delete?:")
+    cursor.execute(
+        "DELETE FROM Book WHERE title = %s",
+            (title,)
+    )
+    libdb.commit()
+
+#Choice menu
+def Menu():
+    while True:
+        print("Welcome to the library!")
+        print("1. Delete a User")
+        print("2. Add a Book")
+        print("3. Delete a Book")
+        choice = input("Choose an option:")
+        match choice:
+            case "1":
+                Delete_User()
+            case "2":
+                Add_Book()
+            case "3":
+                Delete_Book()
+            #case "4":
+                #Show_Book_Selection()
+           #case "5":
+                #Check_Out_Book()
+            #case "6":
+                #Return_Book()
+            #case "7":
+                #Modify_User
 
 def Add_User():
     global rflag, lflag
@@ -43,45 +112,15 @@ def Add_User():
         print("User already exists.")
         return
 
-    Login()
-    
+    print(f"You are logged in as {username}")
+    Menu()
 
-def Menu():
-    while True:
-        print("Welcome to the library!")
-        print("1. Delete a User")
-        print("2. Add a Book")
-        print("3. Delete a Book")
-        choice = input("Choose an option:")
-        match choice:
-            case "1":
-                Delete_User()
-            case "2":
-                print("hi")
+Welcome_Screen() 
 
-Welcome_Screen()
-
-def Login():
-    global current_user
-    current_user = None
-    print("Login")
-    username = input("Username:")
-    upassword = input("Password:")
-
-    cursor.execute(
-        "SELECT * FROM Users WHERE username = %s and upassword = %s",
-        (username, upassword)
-    )
-    #fetches complete line form db
-    name = cursor.fetchone()
-    if name:
-        current_user = username
-        Menu()
-    else:
-        print("Incorrect user or password")
-        Welcome_Screen()  
-
+#Deletes a user, logs you out if it is your own account
+#Recheck, see how to check which cols added
 def Delete_User():
+    global current_user
     username = input("Enter the username of the user you would like to delete:")
     cursor.execute(
         "DELETE FROM Users WHERE username = %s",
@@ -94,17 +133,6 @@ def Delete_User():
     if username == current_user:
         current_user = None
         return
-    
-def Add_Book():
-    title = print("What is the book title?: ")
-    aid = print("What is the author's ID?: ")
-    date_pub = print("What date was it published?: ")
-    author = print("Who is the author?: ")
-
-    cursor.execute(
-        "ADD"
-    )
-
 
 
 ### PSEUDOCODE
@@ -135,7 +163,5 @@ Modify_user()
         update user attributes
 
 Search/View books
-        
-Menu()
-    choose an action & calls the function
+
         '''
